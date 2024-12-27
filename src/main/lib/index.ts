f0f916d30fb50a9f3d59d2e5f52de07a63305bea
Frank Mathy <frankmathy@gmail.com>
2024-12-27 19:09:50 +0100

    Welcome file

--- src/main/lib/index.ts
+++ src/main/lib/index.ts
@@ -1,10 +1,12 @@
-import { appDirectoryName, fileEncoding } from '@shared/constants'
+import { appDirectoryName, fileEncoding, welcomeNoteFileName } from '@shared/constants'
 import { NoteInfo } from '@shared/models'
 import { CreateNote, DeleteNote, GetNotes, ReadNote, WriteNote } from '@shared/types'
 import { dialog } from 'electron'
 import { ensureDir, readdir, readFile, remove, stat, writeFile } from 'fs-extra'
+import { isEmpty } from 'lodash'
 import { homedir } from 'os'
 import path from 'path'
+import welcomeNoteFile from '../../../resources/welcomeNote.md?asset'
 
 export const getRootDir = () => {
   return `${homedir}/${appDirectoryName}`
@@ -18,6 +20,14 @@ export const getNotes: GetNotes = async () => {
     withFileTypes: false
   })
   const notes = notesFileNames.filter((fileName) => fileName.endsWith('.md'))
+
+  if (isEmpty(notes)) {
+    console.info('No notes found, creating a welcome note.')
+    const content = await readFile(welcomeNoteFile, { encoding: fileEncoding })
+    await writeFile(`${rootDir}/${welcomeNoteFileName}`, content, { encoding: fileEncoding })
+    notes.push(welcomeNoteFileName)
+  }
+
   return Promise.all(notes.map(getNoteInfoFromFilename))
 }
 
