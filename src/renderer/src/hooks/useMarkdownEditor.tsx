02f1d338f4038cb6c97f4b0427d55123b1b150ae
Frank Mathy <frankmathy@gmail.com>
2024-12-27 15:54:03 +0100

    Write notes

--- src/renderer/src/hooks/useMarkdownEditor.tsx
+++ src/renderer/src/hooks/useMarkdownEditor.tsx
@@ -1,9 +1,36 @@
-import { selectedNoteAtom } from '@renderer/store'
-import { useAtomValue } from 'jotai'
+import { MDXEditorMethods } from '@mdxeditor/editor'
+import { saveNoteAtom, selectedNoteAtom } from '@renderer/store'
+import { autoSavingTime } from '@shared/constants'
+import { NoteContent } from '@shared/models'
+import { useAtomValue, useSetAtom } from 'jotai'
+import { throttle } from 'lodash'
+import { useRef } from 'react'
 
 const useMarkdownEditor = () => {
   const selectedNote = useAtomValue(selectedNoteAtom)
-  return { selectedNote }
+  const saveNote = useSetAtom(saveNoteAtom)
+  const editorRef = useRef<MDXEditorMethods>(null)
+
+  const handleAutoSaving = throttle(
+    async (content: NoteContent) => {
+      if (!selectedNote) return
+      console.info('Auto saving:', selectedNote.title)
+      await saveNote(content)
+    },
+    autoSavingTime,
+    { leading: false, trailing: true }
+  )
+
+  const handleBlur = async () => {
+    if (!selectedNote) return
+    handleAutoSaving.cancel()
+    const content = editorRef.current?.getMarkdown()
+    if (content != null) {
+      await saveNote(content)
+    }
+  }
+
+  return { editorRef, selectedNote, handleAutoSaving, handleBlur }
 }
 
 export default useMarkdownEditor
--- src/renderer/src/store/index.ts
+++ src/renderer/src/store/index.ts
@@ -1,4 +1,4 @@
-import { NoteInfo } from '@shared/models'
+import { NoteContent, NoteInfo } from '@shared/models'
 import { atom } from 'jotai'
 import { unwrap } from 'jotai/utils'
 
@@ -56,4 +56,23 @@ export const deleteNoteAtom = atom(null, (get, set) => {
     notes.filter((note) => note.title !== selectedNote.title)
   )
   set(selectedNoteIndexAtom, null)
+})
+
+export const saveNoteAtom = atom(null, async (get, set, newContent: NoteContent) => {
+  const notes = get(notesAtom)
+  const selectedNote = get(selectedNoteAtom)
+  if (!selectedNote || !notes) return
+  await window.context.writeNote(selectedNote.title, newContent)
+  set(
+    notesAtom,
+    notes.map((note) => {
+      if (note.title === selectedNote.title) {
+        return {
+          ...note,
+          lastEditTime: Date.now()
+        }
+      }
+      return note
+    })
+  )
 })
